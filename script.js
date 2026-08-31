9e4367a35ff6013f8713df6d930f2fc61ee6f600
pandenitin <pandenitin586@gmail.com>
2026-08-31 23:29:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+class user{
+  constructor(name,email){
+    this.name=name;
+    this.email=email;
+  }
+  viewData()
+  {
+    console.log("website data");
+  }
+}
+let student1=new user("nitin","pandenitin586@gmail.com");
+let student2 =new user("sujit","sujitbhadva@gmail.com");
+let student3 =new user("krish","krishmadar@gmail.com");
